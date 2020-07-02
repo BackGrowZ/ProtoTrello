723b696e39103033caa3066f5fadcc25aec3b020
Anthony Carreta <32708861+BackGrowZ@users.noreply.github.com>
2020-07-02 12:51:40 +0200

    clean code

--- tableau/src/component/404/NoMatchPage.jsx
+++ tableau/src/component/404/NoMatchPage.jsx
@@ -4,21 +4,6 @@ import './style.css'
 export default function NoMatchPage() {
 
   return (
-    // <Fragment>
-    //   <div className="error">
-    //     <div className="container-floud">
-    //       <div className="col-xs-12 ground-color text-center">
-    //         <div className="container-error-404">
-    //           <div className="clip"><div className="shadow"><span className="digit thirdDigit">4</span></div></div>
-    //           <div className="clip"><div className="shadow"><span className="digit secondDigit">0</span></div></div>
-    //           <div className="clip"><div className="shadow"><span className="digit firstDigit">4</span></div></div>
-    //           {/* <div className="msg">OUPS!<span className="triangle"></span></div> */}
-    //         </div>
-    //         <h2 className="h1">Désolé, Nous n'avons pas trouver la page demandé!</h2>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </Fragment>
     <Fragment>
       <div id="notfound">
         <div class="notfound-bg"></div>
@@ -28,7 +13,6 @@ export default function NoMatchPage() {
           </div>
           <h2>Désolé, la page demandé n'a pas était trouvé</h2>
           <a href="/" class="home-btn">Retour sur le site</a>
-          {/* <a href="https://colorlib.com/wp/free-404-error-page-templates/" class="contact-btn">404 by Colorlib</a> */}
           <div class="notfound-social">
             <a href="#Facebook"><i class="fab fa-facebook"></i></a>
             <a href="#Twitter"><i class="fab fa-twitter"></i></a>
